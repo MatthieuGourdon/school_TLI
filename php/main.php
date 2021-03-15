b2dba5885034ba928cbd9688b71e31e2be85e1a4
Pierre Cruveiller <pierre.cruveiller@cpe.fr>
2021-03-15 12:08:23 +0100

    Ajout d'une base de test pour faire des requêtes à la base de données

--- php/main.php
+++ php/main.php
@@ -15,6 +15,39 @@ switch ($_GET["page"]){
         $smarty->display('pathologypage.tpl');
         break;
     default:
-        echo "Non non non";
+        $smarty->display('index.tpl');}
+
+/* Connexion à une base MySQL avec l'invocation de pilote */
+$dsn = 'pgsql:dbname=acudb;host=localhost'; //127.0.0.1
+$user = 'postgres-tli';
+$password = 'tli';
+
+try {
+ $dbh = new PDO($dsn, $user, $password);
+ //echo "Connexion établie ! \n";
+} catch (PDOException $e) {
+ echo "Connexion échouée : " . $e->getMessage();
+}
+
+/* Exécute une requête préparée en liant des variables et valeurs */
+if (empty($_POST))
+ $desc = '';
+else
+ $desc = $_POST["Rsymp"];
+$sth = $dbh->prepare('SELECT s.desc as sdesc, p.desc as pdesc
+ FROM symptome AS s
+ INNER JOIN symptpatho AS sp ON s.ids = sp.ids
+ INNER JOIN patho AS p ON sp.idp = p.idp
+ WHERE s.desc LIKE :desc');
+$sth->bindValue(':desc', "%$desc%"); // avec % pour négliger la suite | ou "${desc}%" pourquoi {}?
+$sth->execute();
+
+$results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row) {
+ foreach ($row as $key => $value) {
+ print $row[$key] . "X\t";
+ }
+ print "Z\n";
 }
 ?>
